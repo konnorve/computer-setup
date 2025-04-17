6824fdb731d09ba3acf2f5c6581b35c74dbb363a
Konnor von Emster <56371517+konnorve@users.noreply.github.com>
2025-04-16 18:23:22 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,30 +6,10 @@ NONINTERACTIVE=1
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Update Homebrew and install applications without prompts
-echo "Updating Homebrew..."
-brew update
+# Install homebrew packages
+curl https://raw.githubusercontent.com/konnorve/mac-setup/refs/heads/main/all.Brewfile | brew bundle --file=-
 
-echo "Installing applications..."
-brew install --cask spotify
-brew install --cask notion
-brew install --cask firefox
-brew install --cask slack
-brew install --cask visual-studio-code
-brew install --cask microsoft-office
-brew install --cask rectangle
-brew install --cask 1password
-brew install --cask cursor
-# brew install --cask google-cloud-sdk
-
-brew install wget
-brew install tree
-brew install imagemagick
-
-# Install Mamba
-brew install --cask mambaforge
-
-echo "Setup complete!"
+mamba init zsh bash
 
 for file in *.yaml
 do
